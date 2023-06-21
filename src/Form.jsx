f9b8bd6339339c1197fce24a13e1294fdb4c84b4
Segura Mendoza Gael Alejandro <108765188+GaelSM@users.noreply.github.com>
2023-06-21 14:44:52 -0600

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,7 @@
 export default function Form({type, currentUser, handleCreate}){
     return (
     <div className="add-comment">
-        <img src={currentUser.image.png} alt="" />
+        <img src={currentUser.image} alt="" />
         <form onSubmit={(e) => handleCreate(e)}>
           <textarea placeholder="Add a comment..." name='text'/>
           <button> {type} </button>
